89591cc2f97d21d41b9870bbb59a7a488178ce93
cosmos45 <zendeomkar45@gmail.com>
2025-04-23 13:37:31 +0100

    test guest

--- scripts/services/axiosService.js
+++ scripts/services/axiosService.js
@@ -1,34 +1,59 @@
 (function () {
-    const backendApi = "https://abubakr.flowpos.co.uk/api/";
-    // Create an Axios instance with baseURL and default headers
-    const axiosServices = axios.create({
-      baseURL: backendApi,
-      headers: { "Content-Type": "application/json" },
-    });
-  
-    
-    // Request interceptor to set the Authorization header
-    axiosServices.interceptors.request.use(
-      async function (config) {
-        let token = localStorage.getItem("token");
-  
-        if (token) {
-          config.headers["Authorization"] = `Bearer ${token}`;
-        }
-        return config;
-      },
-      function (error) {
-        return Promise.reject(error);
+  const backendApi = "https://abubakr.flowpos.me/api/";
+
+  // Generate and store a UID if it doesn't exist
+  function getOrCreateUID() {
+    let uid = localStorage.getItem("uid");
+
+    if (!uid) {
+      if (window.crypto && crypto.randomUUID) {
+        uid = crypto.randomUUID();
+      } else {
+        // Basic fallback using timestamp and random values
+        const timestamp = Date.now().toString(36);
+        const random = Math.random().toString(36).substring(2, 10);
+        uid = `uid-${timestamp}-${random}`;
       }
-    );
-  
+      localStorage.setItem("uid", uid);
+    }
 
+    return uid;
+  }
 
-    // Response interceptor to handle token expiration
-    axiosServices.interceptors.response.use(function (response) {
-      return response.data;
-    });
-  
-    window.axiosServices = axiosServices;
-  })();
-  
+  // Ensure UID is available
+  const uid = getOrCreateUID();
+
+  // Create an Axios instance
+  const axiosServices = axios.create({
+    baseURL: backendApi,
+    headers: { "Content-Type": "application/json" },
+  });
+
+  // Request interceptor to set Authorization and UID headers
+  axiosServices.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+
+      if (uid) {
+        config.headers["X-UID"] = uid; // Custom header for UID
+      }
+
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  // Response interceptor to unwrap data
+  axiosServices.interceptors.response.use(function (response) {
+    return response.data;
+  });
+
+  // Expose axiosServices globally
+  window.axiosServices = axiosServices;
+})();
